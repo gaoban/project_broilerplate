9fdf53d6f21f8325ae5c0d97bbfad191667d9d14
Prai <rknhimura@yahoo.com>
2014-05-25 21:12:16 -0400

    working with static server

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,23 @@ var compass = require('gulp-compass'),
 	livereload = require('gulp-livereload'),
 	gulp = require('gulp'),
 	compass = require('gulp-compass'),
-	gutil = require('gulp-util')
+	gutil = require('gulp-util'),
+	embedlr = require('gulp-embedlr');
 
+
+
+var build = "dev";
+
+/*this setting is not needed for some reason*/
+// gulp.task('server', function(next){
+// 	var connect = require('connect'), server = connect();
+// 	server.use(connect.static()).listen(process.env.PORT || 80, next);
+// });
 
 gulp.task('template', function(){
 	gulp.src('./src/jade/*.jade')
 		.pipe(jade({pretty:true}))
+		.pipe((build==="dev")? embedlr(): gutil.noop())
 		.pipe(gulp.dest('.'));
 });
 
@@ -24,8 +35,12 @@ gulp.task('compass', function(){
 });
 
 gulp.task('watch', function(){
-	gulp.watch('./jade/**/*.jade', ['template']);
+	var server = livereload();
+	gulp.watch('./src/jade/**/*.jade', ['template']);
 	gulp.watch('./src/sass/**/*.scss', ['compass']);
+	gulp.watch(['./css/*.css', './*.html']).on('change', function(file){
+		server.changed(file.path);
+	});
 });
 
 gulp.task('default', ['watch']);
